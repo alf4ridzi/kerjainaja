e674a54d52b99fc50981830ace5c09bd2bc92592
alf4ridzi <aalalfaridzi9@gmail.com>
2025-06-05 08:44:47 +0700

    banyak

--- backend/model/user.go
+++ backend/model/user.go
@@ -4,11 +4,11 @@ import "time"
 
 type User struct {
 	ID        uint   `gorm:"primaryKey" json:"-"`
-	Name      string `gorm:"size:100;not null"`
-	Username  string `gorm:"size:100;not null;uniqueIndex"`
-	Email     string `gorm:"size:100;uniqueIndex;not null"`
+	Name      string `gorm:"size:100;not null" json:"name"`
+	Username  string `gorm:"size:100;not null;uniqueIndex" json:"username"`
+	Email     string `gorm:"size:100;uniqueIndex;not null" json:"email"`
 	Password  string `gorm:"not null" json:"-"`
-	Role      string `gorm:"size:20;default:'user'"`
+	Role      string `gorm:"size:20;default:'user'" json:"role"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
