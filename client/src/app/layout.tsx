f3ed35ac383eaebfb5c70eec8ea463ee46cdf6fa
unknown <yutaagra@gmail.com>
2025-06-07 21:45:21 +0700

    Dev: Added Shadcn UI

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +31,6 @@ export default function RootLayout({
       >
         {children}
         <ToastContainer />
-        
       </body>
     </html>
   );
